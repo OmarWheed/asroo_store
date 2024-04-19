29dafa54249dc21f0f03896e2101963f4b9df878
omar wheed <wheedomar88@gmail.com>
2024-04-19 19:36:08 +0200

    start app

---
+++ lib/asroo_store_app.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class AsrooStoreApp extends StatelessWidget {
+  const AsrooStoreApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Asroo Store',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text("Home"),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:store_app/asroo_store_app.dart';
+
+void main() {
+  runApp(const AsrooStoreApp());
+}
