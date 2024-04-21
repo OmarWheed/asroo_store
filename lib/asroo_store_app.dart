cd221bb1b18d05dd2616181eee2d74829c73174c
omar wheed <wheedomar88@gmail.com>
2024-04-21 18:51:29 +0200

    connectivity plus done

--- lib/asroo_store_app.dart
+++ lib/asroo_store_app.dart
@@ -1,24 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:store_app/core/app/connectivity_controller.dart';
 import 'package:store_app/core/app/env.variables.dart';
+import 'package:store_app/core/common/screens/no_network_screnns.dart';
 
 class AsrooStoreApp extends StatelessWidget {
   const AsrooStoreApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Asroo Store',
-      debugShowCheckedModeBanner: EnvVariable.instance.debugMode,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: const Text("Home"),
-        ),
-      ),
+    return ValueListenableBuilder(
+      valueListenable: ConnectivityController.instance.isConnected,
+      builder: (_, value, child) {
+        if (value) {
+          return MaterialApp(
+            builder: (_, widget) {
+              return Scaffold(
+                body: Builder(
+                  builder: (context) {
+                    ConnectivityController.instance.init();
+                    return widget!;
+                  },
+                ),
+              );
+            },
+            title: 'Asroo Store',
+            debugShowCheckedModeBanner: EnvVariable.instance.debugMode,
+            theme: ThemeData(
+              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+              useMaterial3: true,
+            ),
+            home: Scaffold(
+              appBar: AppBar(
+                centerTitle: true,
+                title: const Text("Home"),
+              ),
+            ),
+          );
+        } else {
+          return MaterialApp(
+            debugShowCheckedModeBanner: EnvVariable.instance.debugMode,
+            title: 'No Internet',
+            home: const NoNetworkScreen(),
+          );
+        }
+      },
     );
   }
 }
