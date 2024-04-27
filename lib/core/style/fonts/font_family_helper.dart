abe8e14f5733f22124c13c9bebb13e380ef42059
omar wheed <wheedomar88@gmail.com>
2024-04-27 18:50:18 +0300

    custom widget

--- lib/core/style/fonts/font_family_helper.dart
+++ lib/core/style/fonts/font_family_helper.dart
@@ -2,4 +2,13 @@ class FontFamilyHelper {
   const FontFamilyHelper._();
   static const String cairoArabic = "Cairo";
   static const String poppinsEnglish = "Poppins";
+
+  static String geLocalozedFontFamily() {
+    const currentLanguage = "ar";
+    if (currentLanguage == "ar") {
+      return cairoArabic;
+    } else {
+      return poppinsEnglish;
+    }
+  }
 }
